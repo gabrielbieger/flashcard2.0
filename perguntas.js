0acdac1430f5d7dd313f857af17b9a087df29e04
gabrielbieger <130661115+gabrielbieger@users.noreply.github.com>
2024-09-18 19:45:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,58 @@
+criaCartao(
+    'Piada',
+    'por que o batman foi a igreja ?',
+    'para ver um bat-izo.'
+)
+
+criaCartao(
+    'Piada',
+    'como se pronuncia hospital em japonês ?',
+    'OTIKURO OIMATO'
+)
+
+criaCartao(
+    'Piada',
+    'por que o Adão não tinha sogra ?',
+    'porque ele morava no paraíso.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Tchau em Inglês?',
+    'Tchau em ingles é bye.'
+)
+
+criaCartao(
+    'Química',
+    'O que e H2O',
+    'É Agua.'
+)
+criaCartao(
+    'Programação',
+    'O que é BIT',
+    'é a menor unidade de medida de transmissão de dados.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Rússia?',
+    'A capital da Rússia é Moscou'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Nova Pergunta',
+    'Isso é um teste?',
+    'SIM'
+)
